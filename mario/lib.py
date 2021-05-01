d0f93af4624436f919e21ad7686ae5dc628dd45d
felix faehnrich <felix@faehnrich.de>
2021-05-01 13:20:30 +0200

    Now with the Script

--- mario/lib.py
+++ mario/lib.py
@@ -1,2 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from termcolor import colored
+
 def try_me():
-    return "Good Try!"
+    name = input('What is your name? ')
+    print()
+    return f"It's me! {colored(name,'red')}!"
